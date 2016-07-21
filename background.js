9fb8f0f907dccfb570c4df076b74085826805473
Erick Fleming <efleming969@gmail.com>
2016-07-21 06:56:39 -0500

    changed impl to update multiple tabs

--- background.js
+++ background.js
@@ -1,32 +1,51 @@
-var connections = null
+var ws = null
+var tabs = []
 
-var createConnection = function( id, callback ) {
-
-  var ws = new WebSocket('ws://localhost:8080')
-
-  ws.onmessage = function( event ) {
-    console.log( 'message:' + event.data )
-    if ( event.data == 'reload' ) {
-      chrome.tabs.reload( id )
-    }
-  }
-
-  ws.onopen = function( event ) {
-    callback( ws )
-  }
-
-  ws.onclose = function( event ) {
-    chrome.browserAction.setIcon( { path: 'icon-off-19.png' }, function() {
-      connection = null
-    } )
-  }
-
+var isLocalHost = function( url ) {
+  return url != undefined && url.indexOf( 'http://localhost:8080' ) == 0
 }
 
 chrome.browserAction.onClicked.addListener( function ( tab ) {
-  createConnection( tab.id, function( ws ) {
-    chrome.browserAction.setIcon( { path: 'icon-on-19.png' }, function() {
-      connections.push( ws )
-    } )
-  } )
+
+  if ( ws == null || ws.readyState == WebSocket.CLOSED ) {
+
+    ws = new WebSocket('ws://localhost:8080')
+
+    ws.onmessage = function( event ) {
+      if ( event.data == 'reload' ) {
+        tabs.forEach( function( tabId ) {
+          chrome.tabs.reload( tabId )
+        } )
+      }
+    }
+
+    ws.onopen = function( event ) {
+      chrome.browserAction.setIcon( { path: 'icon-on-19.png' }, function() {
+        console.log( 'connected to localhost' )
+      } )
+    }
+
+    ws.onclose = function( event ) {
+      chrome.browserAction.setIcon( { path: 'icon-off-19.png' }, function() {
+        console.log( 'diconnected from localhost' )
+      } )
+    }
+  }
+  else if ( ws.readyState == WebSocket.OPEN ) {
+    ws.close()
+  }
+
 } )
+
+chrome.tabs.onUpdated.addListener( function( tabId, changeInfo ) {
+  if ( isLocalHost( changeInfo.url ) ) {
+    tabs.push( tabId )
+  }
+} )
+
+chrome.tabs.onRemoved.addListener( function( removed_tabId ) {
+  tabs.splice( tabs.findIndex( function( tabId, index ) {
+    return tabId == removed_tabId
+  } ), 1 )
+} )
+
